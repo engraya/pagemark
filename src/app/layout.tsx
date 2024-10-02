483ce63a0c2e92b5d987a592d6741b92e1f020e5
Engraya <ahmadyakubuaya@gmail.com>
2024-10-02 09:14:57 +0100

    Added some styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,9 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Lora } from 'next/font/google'
 import "./globals.css";
 import { ThemeProvider } from "../../components/ThemeProvider";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Lora({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -27,8 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={inter.className}>
       <ThemeProvider
             attribute="class"
             defaultTheme="system"
---
+++ src/components/Header.tsx
@@ -0,0 +1,68 @@
+"use client"
+import { motion } from 'framer-motion'
+import Link from 'next/link'
+import { ThemeToggler } from '../../components/ThemeToggler'
+export default function Header() {
+  return (
+   <header style={headerStyle}>
+    <motion.div style={leftContainerStyle}
+    transition={{type:'spring',damping:18,mass:0.75}}
+    initial={{opacity:0,x:-1000}} animate={{opacity:1,x:0}}
+>
+    <h1 style={bookTitleStyle}>PageMark</h1>
+
+     
+    </motion.div>
+
+    <motion.div style={rightContainerStyle} 
+    transition={{type:'spring',damping:18,mass:0.75}}
+    initial={{opacity:0,x:1000}} animate={{opacity:1,x:0}}
+    >
+    </motion.div>
+   </header>
+  )
+}
+
+
+const headerStyle = {
+    display:'flex',
+    justifyContent:'space-between',
+    alignItems:'center',
+    padding:'1rem 1re 1rem 0',
+    color:'#fff'
+}
+
+const leftContainerStyle = {
+    display:'flex',
+    alignItems:'center'
+}
+
+const bookTitleStyle = {
+    marginRight:'2rem',
+    color:'#000'
+}
+
+const searchInputStyle = {
+    padding:"0.7rem 1rem",
+    marginLeft:'3.6rem',
+    borderRadius:'70px',
+    backgroundColor:'#e2e8f0',
+    border:'2px solid #000',
+    minWidth:'320px',
+}
+
+const rightContainerStyle ={
+    display:'flex',
+    alignItems:'center'
+} 
+
+const avatarLinkStyle = {
+    marginRight:'1rem'
+}
+
+const avatarStyle ={
+    width:"40px",
+    height:'40px',
+    borderRadius:'50%'
+}
+
